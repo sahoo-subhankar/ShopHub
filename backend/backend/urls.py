849eae7be9d7882798e47d9572c89781b54a6c36
Subhankar Sahoo <sahoosubha2020@gmail.com>
2023-10-28 16:51:42 +0530

    New Files Added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,11 +10,8 @@ urlpatterns = [
     path('api/products/', include("myapp.urls.product_urls")),
     path('api/users/', include("myapp.urls.user_urls")),
     path('api/orders/', include("myapp.urls.order_urls")),
-    
-    # YOUR PATTERNS
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
