f934151fa0cf46f86201c404ad1b319a43e00525
Subhankar Sahoo <sahoosubha2020@gmail.com>
2023-10-29 21:15:38 +0530

    Modified Files

--- frontend/src/components/Paginator.js
+++ frontend/src/components/Paginator.js
@@ -7,23 +7,49 @@ function Paginator({ pages, page, keyword = '', isAdmin = false }) {
         keyword = keyword.split('?keyword=')[1].split('&')[0];
     }
 
-    return (pages > 1 && (
-        <Pagination>
-            {[...Array(pages).keys()].map((x) => (
-                <LinkContainer
-                    key={x + 1}
-                    to={{
-                        pathname: !isAdmin ? '/' : '/admin/productlist',
-                        search: `?keyword=${keyword}&page=${x + 1}`,
-                    }}>
-                    <Pagination.Item disabled={x + 1 === page} >
-                        {x + 1}
-                    </Pagination.Item>
-                </LinkContainer>
-            ))}
-        </Pagination>
-    )
-    )
+    return (
+        pages > 1 && (
+            <Pagination>
+                {page > 1 && (
+                    <LinkContainer
+                        to={{
+                            pathname: !isAdmin ? '/' : '/admin/productlist',
+                            search: `?keyword=${keyword}&page=${page - 1}`,
+                        }}>
+                        <Pagination.Item>
+                            ←
+                        </Pagination.Item>
+                    </LinkContainer>
+                )}
+
+                {[...Array(pages).keys()].map((x) => (
+                    <LinkContainer
+                        key={x + 1}
+                        to={{
+                            pathname: !isAdmin ? '/' : '/admin/productlist',
+                            search: `?keyword=${keyword}&page=${x + 1}`,
+                        }}>
+                        <Pagination.Item disabled={x + 1 === page}>
+                            {x + 1}
+                        </Pagination.Item>
+                    </LinkContainer>
+                ))}
+
+                {page < pages && (
+                    <LinkContainer
+                        to={{
+                            pathname: !isAdmin ? '/' : '/admin/productlist',
+                            search: `?keyword=${keyword}&page=${page + 1}`,
+                        }}>
+                        <Pagination.Item>
+                            →
+                        </Pagination.Item>
+                    </LinkContainer>
+                )}
+            </Pagination>
+        )
+    );
 }
 
 export default Paginator;
+
